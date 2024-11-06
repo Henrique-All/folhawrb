32fae11ae99dcbc925d0b5c16521999b4acd2201
CH <carlinhos.biocraft@gmail.com>
2024-11-06 17:31:54 -0300

    feat: search link

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,19 @@
-function renderLinks() {
+function filtrarPorNome(criterio) {
+  return (array) =>
+    array.filter((item) =>
+      item.nome.toLowerCase().includes(criterio.toLowerCase())
+    );
+}
+
+// Função para renderizar
+function renderLinks(filtro) {
+  // Filtrar os dados
+  const prefeiturasFiltradas = filtrarPorNome(filtro)(prefeituras);
+  const camarasFiltradas = filtrarPorNome(filtro)(camaras);
+  const outrosFiltrados = filtrarPorNome(filtro)(outros);
+
   // Renderizar prefeituras
-  document.getElementById("prefeituras").innerHTML = prefeituras
+  document.getElementById("prefeituras").innerHTML = prefeiturasFiltradas
     .map((prefeitura) => {
       return `
         <a class="card" href="${prefeitura.url}" target="_blank">
@@ -13,20 +26,20 @@ function renderLinks() {
     .join("");
 
   // Renderizar câmaras
-  document.getElementById("camaras").innerHTML = camaras
+  document.getElementById("camaras").innerHTML = camarasFiltradas
     .map((camara) => {
       return `
         <a class="card" href="${camara.url}" target="_blank">
           <div class="card-header">
             <p>GESTÂO PUBLICA</p>
           </div>
-          <p class="name" >${camara.nome}</p>
+          <p class="name">${camara.nome}</p>
         </a>`;
     })
     .join("");
 
   // Renderizar outros
-  document.getElementById("outros").innerHTML = outros
+  document.getElementById("outros").innerHTML = outrosFiltrados
     .map((outros) => {
       return `
         <a class="card" href="${outros.url}" target="_blank">
@@ -40,4 +53,10 @@ function renderLinks() {
     .join("");
 }
 
-renderLinks();
+// Chamar renderLinks
+renderLinks("");
+
+// Adicionar evento de input
+document.getElementById("buscar-nome").addEventListener("input", (e) => {
+  renderLinks(e.target.value); // Atualiza a renderização
+});
