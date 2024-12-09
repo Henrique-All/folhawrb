3c444dc7ec39e6e35f47535facf4f1a9cb9c4b95
CH <carlinhos.biocraft@gmail.com>
2024-12-09 16:37:22 -0300

    feat: new linl

--- src/js/db/Camaras.js
+++ src/js/db/Camaras.js
@@ -6,6 +6,10 @@ const camaras = [
     url: "http://gestaopublica.camara.BuritiAlegre.bsit-br.com.br/login.jsf",
   },
   {
+    nome: "Camara Buriti de Goiás",
+    url: "http://gestaopublica.camara.buritidegoias.bsit-br.com.br/login.jsf ",
+  },
+  {
     nome: "Camara Bom Jesus",
     url: "http://gestaopublica.camara.bomjesus.bsit-br.com.br/login.jsf",
   },
