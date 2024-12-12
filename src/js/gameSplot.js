74492f4f3ddba4aa358060cabb2536d2f6f096da
CH <carlinhos.biocraft@gmail.com>
2024-12-12 16:38:54 -0300

    feat: novo jogo

--- src/js/gameSplot.js
+++ src/js/gameSplot.js
@@ -5,16 +5,19 @@ const button = document.getElementById("open-modal");
 const closeModal = document.getElementById("close-modal");
 const closeP = document.getElementById("close-gameP");
 const closeV = document.getElementById("close-gameV");
+const closeS = document.getElementById("close-gameS");
 const div = document.getElementById("modal");
 const paciencia = document.getElementById("paciencia");
 const velha = document.getElementById("velha");
+const snaker = document.getElementById("snake");
 const btPaciencia = document.getElementById("bt-paciencia");
 const btVelha = document.getElementById("bt-velha");
+const btSnaker = document.getElementById("bt-snake");
 
 // Adiciona o evento de clique ao botão
 button.addEventListener("click", () => {
   clickCount++; // Incrementa o contador
-  if (clickCount === 7) {
+  if (clickCount === 1) {
     div.classList.add("active"); // Remove a classe 'hidden' da div
   }
 });
@@ -22,6 +25,7 @@ button.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
   div.classList.remove("active");
   paciencia.classList.add("hidden");
+  snaker.classList.add("hidden");
   velha.classList.add("hidden");
   btVelha.classList.remove("hidden");
   btPaciencia.classList.remove("hidden");
@@ -32,11 +36,20 @@ btPaciencia.addEventListener("click", () => {
   paciencia.classList.remove("hidden");
   btVelha.classList.add("hidden");
   btPaciencia.classList.add("hidden");
+  btSnaker.classList.add("hidden");
 });
 
 btVelha.addEventListener("click", () => {
   velha.classList.remove("hidden");
   btVelha.classList.add("hidden");
+  btPaciencia.classList.add("hidden");
+  btSnaker.classList.add("hidden");
+});
+
+btSnaker.addEventListener("click", () => {
+  snaker.classList.remove("hidden");
+  btVelha.classList.add("hidden");
+  btSnaker.classList.add("hidden");
   btPaciencia.classList.add("hidden");
 });
 
@@ -44,10 +57,19 @@ closeP.addEventListener("click", () => {
   paciencia.classList.add("hidden");
   btVelha.classList.remove("hidden");
   btPaciencia.classList.remove("hidden");
+  btSnaker.classList.remove("hidden");
 });
 
 closeV.addEventListener("click", () => {
   velha.classList.add("hidden");
   btVelha.classList.remove("hidden");
   btPaciencia.classList.remove("hidden");
+  btSnaker.classList.remove("hidden");
+});
+
+closeS.addEventListener("click", () => {
+  snaker.classList.add("hidden");
+  btVelha.classList.remove("hidden");
+  btPaciencia.classList.remove("hidden");
+  btSnaker.classList.remove("hidden");
 });
