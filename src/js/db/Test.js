55ec9621a6c22f01a418bd1d0ff1604496520c24
CH <carlinhos.biocraft@gmail.com>
2024-12-12 08:12:49 -0300

    new upload server test

--- src/js/db/Test.js
+++ src/js/db/Test.js
@@ -62,9 +62,9 @@ const test = [
   {
     nome: namesMunicipios[0].municipios[2],
     url: url[0].server[2],
-    tag: tags[0].off,
+    tag: tags[0].on,
     tag3: "",
-    color: color[0].off,
+    color: color[0].on,
   },
   {
     nome: namesMunicipios[0].municipios[3],
@@ -90,16 +90,16 @@ const test = [
   {
     nome: namesMunicipios[0].municipios[6],
     url: url[0].server[6],
-    tag: tags[0].on,
+    tag: tags[0].off,
     tag3: "",
-    color: color[0].on,
+    color: color[0].off,
   },
   {
     nome: namesMunicipios[0].municipios[7],
     url: url[0].server[7],
-    tag: tags[0].off,
+    tag: tags[0].on,
     tag3: "",
-    color: color[0].off,
+    color: color[0].on,
   },
   {
     nome: namesMunicipios[0].municipios[8],
