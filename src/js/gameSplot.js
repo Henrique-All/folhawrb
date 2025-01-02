9ae8881fb8def287843e3620d88f7dc1a48bc84b
CH <carlinhos.biocraft@gmail.com>
2025-01-02 07:33:44 -0800

    games is work

--- src/js/gameSplot.js
+++ src/js/gameSplot.js
@@ -18,7 +18,7 @@ const btSnaker = document.getElementById("bt-snake");
 button.addEventListener("click", () => {
   clickCount++; // Incrementa o contador
   if (clickCount === 5) {
-    alert("Trabalhar que e bom nada em"); // Remove a classe 'hidden' da div
+    div.classList.add("active"); // Remove a classe 'hidden' da div
   }
 });
 
